d992793283a1d06c99d9536376b9cd84402e4d10
xinhnguyen31231021729 <xinhnguyen.31231021729@st.ueh.edu.vn>
2024-08-25 21:29:20 +0700

    Add files via upload

---
+++ BaitapStrings.cs
@@ -0,0 +1,31 @@
+﻿internal class Baitap_string
+{
+    static void Main(string[] args)
+    {
+        //Baiso01();
+        //Baiso02();
+        
+    }
+
+    static void Baiso01()
+    {string str; 
+    Console.Write("\n\nAccept a string from the keyboard:\n");
+        Console.Write("Input the string: ");
+        str = Console.ReadLine(); 
+        Console.Write("The string you entered is: {0}\n", str);
+        }
+
+    static void Baiso02()
+    {string str; 
+    int l = 0; 
+
+    Console.Write("\n\nFind the length of a string:\n");
+        Console.Write("Input the string: ");
+        str = Console.ReadLine(); 
+        foreach (char chr in str)
+        {
+            l += 1; 
+        }
+    Console.Write("Length of the string is: {0}\n\n", l);
+}
+}
