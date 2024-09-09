168f459154343844d106b6b4adc1a1f3c62c6df8
xinhnguyen31231021729 <xinhnguyen.31231021729@st.ueh.edu.vn>
2024-09-09 12:30:09 +0700

    Add files via upload

---
+++ Section04.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //baitap_01();
+        //baitap_02();
+        //baitap_03();
+        baitap_04();
+    }
+
+
+    static void baitap_01()
+    {
+        Console.WriteLine("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("It's an even number.");
+        }
+        else
+        {
+            Console.WriteLine("It's an odd number.");
+        }
+    }
+
+    static void baitap_02()
+    {
+        Console.WriteLine("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        if ((num1 > num2) && (num1 > num3))
+        {
+            Console.WriteLine("The largest number is: {0}", num1);
+        }
+        else if (num2 > num3)
+        {
+            Console.WriteLine("The largest number is: {0}", num2);
+        }
+        else
+        {
+            Console.WriteLine("The largest number is: {0}", num3);
+        }
+    }
+
+    static void baitap_03()
+    {
+        Console.WriteLine("Enter x: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("The point is in the 1st quadrant");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("The point is in the 2nd quadrant");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("The point is in the 3rd quadrant");
+        }
+        else if(x > 0 && y < 0)
+        {
+            Console.WriteLine("The point is in the 4th quadrant");
+        }
+
+    }
+
+    static void baitap_04()
+    {
+        Console.WriteLine("Nhập số đo cạnh a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập số đo cạnh b: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhập số đo cạnh c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        if(a>b&& b>c)
+
+    }
+}
